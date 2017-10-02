fb29f17bc832b4942d1ef7b54ebec1ac98dab9e3
Unknown <kelseyhu2425@gmail.com>
2017-10-02 15:47:23 -0400

    Primary set up

---
+++ Assets/Scripts/DepressionIndicator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepressionIndicator : MonoBehaviour {
+
+	float timeRemaining = 20f;
+	int numberPercentage = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		Debug.Log("Depression Level: " + numberPercentage.ToString() + "0%");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeRemaining -= Time.deltaTime;
+
+		if (timeRemaining < 0.99f)
+		{
+			numberPercentage -= 1;
+			Debug.Log("Depression Level: " + numberPercentage.ToString() + "0%");
+
+			timeRemaining = 20f;
+		}
+		
+		if (numberPercentage < 0)
+		{
+			Lose();
+		}
+		
+	}
+
+	void Lose()
+	{
+		Debug.Log("You Lose");
+	}
+
+	void OnTriggerEnter(Collider itemInfo)
+	{
+		if (itemInfo.CompareTag("Clue"))
+		{
+			numberPercentage += 1;
+			Debug.Log("You find a clue and become slighly less depressed! " +
+					  "Depression Level: " + numberPercentage.ToString() + "0%");
+			timeRemaining = 20f;
+		}
+		else if (itemInfo.CompareTag("BadStuff"))
+		{
+			numberPercentage -= 1;
+			Debug.Log("You find something very bad and become more depressed! " +
+					  "Depression Level: " + numberPercentage.ToString() + "0%");
+			timeRemaining = 20f;
+		}
+
+		itemInfo.gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+
+	Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CharacterController cController = GetComponent<CharacterController>();
+
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		moveDirection = new Vector3(-vertical, 0f, horizontal);
+		if (moveDirection.magnitude > 1f)
+		{
+			moveDirection = Vector3.Normalize(moveDirection);
+		}
+
+		moveDirection *= moveSpeed;
+
+		cController.Move(moveDirection * Time.deltaTime);
+
+	}
+}
