655cae4e6f24375b74e0764c32c9caf88f5db2e8
Unknown <kelseyhu2425@gmail.com>
2017-10-22 15:50:23 -0400

    Can examine certain objects twice with a different outcome

--- Assets/Scripts/DepressionIndicator.cs
+++ Assets/Scripts/DepressionIndicator.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 using UnityEngine.UI;
 
-public class DepressionIndicator : MonoBehaviour {
+public class DepressionIndicator : MonoBehaviour
+{
 
 	public Text UIText;
 	public Text insanityIndicator;
@@ -12,16 +13,20 @@ public class DepressionIndicator : MonoBehaviour {
 	float timeRemaining = 20f;
 	int numberPercentage = 0;
 
+	public bool interactedWithNewspaper = false;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 		insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
-		
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 
 		timeRemaining -= Time.deltaTime;
 
@@ -51,59 +56,46 @@ public class DepressionIndicator : MonoBehaviour {
 	{
 		UIText.text = "Press 'SPACE' to examine " + itemInfo.gameObject.name;
 
-		if (Input.GetKey(KeyCode.Space))
+		if (itemInfo.CompareTag("Couch"))
 		{
-			if (itemInfo.CompareTag("Couch"))
+			insanityDecreases(itemInfo.gameObject, 1,
+							  "Normally your cat would be sleeping on the couch." +
+							  "\nBut he is not here today." +
+							  "\nMaybe hiding in some corners?\nHe loves hiding.");
+		}
+		else if (itemInfo.CompareTag("AC"))
+		{
+			insanityIncreases(itemInfo.gameObject, 1,
+							  "You see cockroach bodies piling up behind the AC." +
+							  "\nMost cockroaches in your house come from the kitchen." +
+							  "\nWhy are they here? What has your cat been doing?");
+		}
+		else if (itemInfo.CompareTag("Table"))
+		{
+			insanityIncreases(itemInfo.gameObject, 1,
+							  "A dead rat on the dining table. Gross." +
+							  "\nSeems like the blood is still fresh.");
+		}
+		else if (itemInfo.CompareTag("Newspaper"))
+		{
+
+			if (interactedWithNewspaper == false)
 			{
-				if (numberPercentage == 0)
-				{
-					numberPercentage = 0;
-				}
-				else {
-					numberPercentage -= 1;
-				}
-				UIText.text = "Normally your cat would be sleeping on the couch.\nBut he is not here today." +
-					"\nMaybe hiding in some corners?\nHe loves hiding.";
-				insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
-				timeRemaining = 10f;
-
-				itemInfo.gameObject.GetComponent<BoxCollider>().enabled = false;
+				insanityDecreases(itemInfo.gameObject, 1,
+								  "You see scratched newpaper scattering on the floor, covered with bloodmarks" +
+								  "\nWhere did your cat get all these newspapers?");
+				
+				StartCoroutine(restoreTriggerAfterSeconds(5, itemInfo.gameObject, interactedWithNewspaper));
 			}
-			else if (itemInfo.CompareTag("AC"))
-			{
-				numberPercentage += 1;
-				UIText.text = "You see cockroach bodies piling up behind the AC.\nMost cockroaches in your house come from the kitchen." +
-					"\nWhy are they here?\nWhat has your cat been doing?";
-				insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
-				timeRemaining = 10f;
-
-				itemInfo.gameObject.GetComponent<BoxCollider>().enabled = false;
+			else {
+				insanityIncreases(itemInfo.gameObject, 1,
+				  "Under the newspapers you find a sticky note." +
+				  "\nThe password to the safe is written on it.");
 			}
-			else if (itemInfo.CompareTag("Table"))
-			{
-				numberPercentage += 2;
-				UIText.text = "A dead rat on the dining table. Gross.\nSeems like the blood is still fresh.";
-				insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
-				timeRemaining = 10f;
-
-				itemInfo.gameObject.GetComponent<BoxCollider>().enabled = false;
-			}
-			else if (itemInfo.CompareTag("Newspaper"))
-			{
-				numberPercentage += 1;
-				UIText.text = "Under the newspapers you find a sticky note." +
-					"\nThe password to the safe is written on it.";
-				insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
-				timeRemaining = 10f;
-
-				itemInfo.gameObject.GetComponent<BoxCollider>().enabled = false;
-			}
-			else if (itemInfo.CompareTag("Cat"))
-			{
-				UIText.text = "You've found the cat! You win!";
-			}
-
-
+		}
+		else if (itemInfo.CompareTag("Cat"))
+		{
+			UIText.text = "You've found the cat! You win!";
 		}
 
 	}
@@ -120,4 +112,47 @@ public class DepressionIndicator : MonoBehaviour {
 		UIText.text = "";
 	}
 
+	IEnumerator restoreTriggerAfterSeconds(int sec, GameObject gameObj, bool interacted)
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			interacted = true;
+			yield return new WaitForSeconds(sec);
+			gameObj.GetComponent<BoxCollider>().enabled = true;
+		}
+	}
+
+	void insanityIncreases(GameObject obj, int incrementNum, string displayText)
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			numberPercentage += incrementNum;
+			UIText.text = displayText;
+			insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
+			timeRemaining = 10f;
+
+			obj.GetComponent<BoxCollider>().enabled = false;
+		}
+	}
+
+	void insanityDecreases(GameObject obj, int decrementNum, string displayText)
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			if (numberPercentage <= decrementNum)
+			{
+				numberPercentage = 0;
+			}
+			else {
+				numberPercentage -= decrementNum;
+			}
+			UIText.text = displayText;
+			insanityIndicator.text = "Insanity: " + numberPercentage.ToString() + "0%";
+			timeRemaining = 10f;
+
+			obj.GetComponent<BoxCollider>().enabled = false;
+		}
+	}
 }
+
+
